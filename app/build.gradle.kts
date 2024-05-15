70dbc59549141f8300eeace227d56b15ff2346ba
XiangLelam <xianglelam2004@gmail.com>
2024-05-15 16:26:33 +0800

    changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,4 +95,5 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
 }
